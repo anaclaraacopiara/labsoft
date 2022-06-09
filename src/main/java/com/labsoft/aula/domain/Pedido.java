f237a06c7ab4198d11495fb6890bfc8cd689fdb0
anaclaraacopiara <anaclaraacopiara@gmail.comanaclaraacopiara@gmail.com>
2022-06-08 23:41:59 -0300

    ItemPedido e itemPedidoPK

--- src/main/java/com/labsoft/aula/domain/Pedido.java
+++ src/main/java/com/labsoft/aula/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.labsoft.aula.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -31,7 +34,8 @@ public class Pedido implements Serializable {
 @ManyToOne
 @JoinColumn(name="cliente_id")
 	private Cliente cliente;
-
+@OneToMany(mappedBy = "id.pedido")
+  private Set<ItemPedido> items = new HashSet<>();
 	public Pedido() {
 
 	}
@@ -83,7 +87,13 @@ public class Pedido implements Serializable {
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+	public Set<ItemPedido> getItems() {
+		return items;
+	}
 
+	public void setItems(Set<ItemPedido> items) {
+		this.items = items;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -108,5 +118,7 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 
 }
