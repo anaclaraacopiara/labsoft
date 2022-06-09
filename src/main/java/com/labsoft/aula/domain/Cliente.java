837f38211a4816088ce20a406efa260f339716fa
anaclaraacopiara <anaclaraacopiara@gmail.comanaclaraacopiara@gmail.com>
2022-06-08 22:25:25 -0300

    Pagamento, Pedido e EstadoPagamento

--- src/main/java/com/labsoft/aula/domain/Cliente.java
+++ src/main/java/com/labsoft/aula/domain/Cliente.java
@@ -31,9 +31,17 @@ public class Cliente implements Serializable {
 	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
+	
+	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
+	
+	
 
 	public Cliente() {
 
@@ -104,6 +112,13 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
